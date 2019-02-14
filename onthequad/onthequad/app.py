9fb966f286d7ae34024c40543f0c56b3a77486ad
ThienNgo Nguyen Le <thienngole@ThienNgos-MacBook-Pro.local>
2019-02-14 13:54:51 -0700

    Setup DB

--- onthequad/onthequad/app.py
+++ onthequad/onthequad/app.py
@@ -1,6 +1,20 @@
+import pyrebase
 import falcon
 from .events import Event
 from .users import User
+
+config = {
+'apiKey': "AIzaSyBEaQNRbDl0qmfTBORJ-4gbMdiBaSm3Q_o",
+    'authDomain': "on-the-quad.firebaseapp.com",
+    'databaseURL': "https://on-the-quad.firebaseio.com",
+    'projectId': "on-the-quad",
+    'storageBucket': "on-the-quad.appspot.com",
+    'messagingSenderId': "535666577502"
+}
+firebase = pyrebase.initialize_app(config)
+
+authe = firebase.auth()
+database = firebase.database()
 
 api = application = falcon.API
 
