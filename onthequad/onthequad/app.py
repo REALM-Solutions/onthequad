97c3e3eb06a3c4afcb1e67bd60e3ce9afef61f12
John Sanders <jbsanders92@gmail.com>
2019-02-19 13:25:41 -0700

    Added them parens
    You can now launch the localserver. Yay

--- onthequad/onthequad/app.py
+++ onthequad/onthequad/app.py
@@ -16,7 +16,7 @@ firebase = pyrebase.initialize_app(config)
 authe = firebase.auth()
 database = firebase.database()
 
-api = application = falcon.API
+api = application = falcon.API()
 
 api.add_route('/events', Event())
 api.add_route('/users', User())
