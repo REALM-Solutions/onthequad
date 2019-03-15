792e650fab62d76505e1d9002362bb07ac23c241
John Sanders <jbsanders92@gmail.com>
2019-03-15 01:42:39 -0600

    Changes made to app.py for api
    Changed middleware to allow react native to suck less.

--- onthequad/onthequad/app.py
+++ onthequad/onthequad/app.py
@@ -2,8 +2,18 @@ import falcon
 from .EventEndPoints import EventEndPoints
 from .dummyusers import DumUsers
 from .UserEndPoints import UserEndPoints
+import requests
 
-api = application = falcon.API()
+ALLOWED_ORIGINS = ["http://localhost:8000"]
+
+class CorsMiddleware(object):
+
+    def process_request(self, request, response):
+        origin = request.get_header('Origin')
+        if origin in ALLOWED_ORIGINS:
+            response.set_header('Access-Control-Allow-Origin', origin)
+
+api = application = falcon.API(middleware=[CorsMiddleware()])
 
 api.add_route('/events', EventEndPoints())
 api.add_route('/dummyusers', DumUsers())
