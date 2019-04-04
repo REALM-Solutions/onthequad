39edc3dcb75f7a34755e3d2f435276165e8a0aa0
ThienNgo Nguyen Le <thienngole@ThienNgos-MacBook-Pro.local>
2019-04-03 23:39:53 -0600

    send 'EMAIL_EXIST' message back when the email is used.

--- onthequad/onthequad/app.py
+++ onthequad/onthequad/app.py
@@ -2,6 +2,7 @@ import falcon
 from .EventEndPoints import EventEndPoints
 from .dummyusers import DumUsers
 from .UserEndPoints import UserEndPoints
+from .SignInEndPoints import SignInEndPoints
 from falcon_cors import CORS
 
 cors = CORS(allow_all_origins=['http://localhost:3000'],
@@ -13,3 +14,4 @@ api = application = falcon.API(middleware=[cors.middleware])
 api.add_route('/events', EventEndPoints())
 api.add_route('/dummyusers', DumUsers())
 api.add_route('/users', UserEndPoints())
+api.add_route('/signin', SignInEndPoints())
