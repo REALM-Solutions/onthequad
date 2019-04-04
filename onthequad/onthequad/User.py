91839cc9223aba4964bc12e25f4ab0b775af4e86
Jack Kolb <Jack.Kolb@Workiva.com>
2019-04-04 17:02:39 -0600

    removed double id storage

--- onthequad/onthequad/User.py
+++ onthequad/onthequad/User.py
@@ -1,11 +1,9 @@
 class User:
     def __init__(self, id, firstName, lastName, email, userName, photoUrl):
-        self.id = id
         self.firstName = firstName
         self.lastName = lastName
         self.email = email
         self.userName = userName
         self.photoUrl = photoUrl
-        
-        
-        
+
+
